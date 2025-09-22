c8e8dced1a32ad252864f25ad77ef1b527505393
shailesh-dianapps <shailesh.mangal@dianapps.com>
2025-09-22 11:17:27 +0530

    Commit

--- cron/leaveUpdater.js
+++ cron/leaveUpdater.js
@@ -1,14 +1,16 @@
 const cron = require('node-cron') 
 const User = require('../models/user');
 
-// Schedule: Run at 12:00 AM on 1st day of every month
-// Cron job: 1st of every month at midnight
-// Format: minute hour day-of-month month day-of-week
-// 0 minute
-// 0 hour
-// 1 → the 1st day of the month
-// * any month
-// * any day of the week
+/**
+    Schedule: Run at 12:00 AM on 1st day of every month
+    Cron job: 1st of every month at midnight
+    Format: minute hour day-of-month month day-of-week
+    0 minute
+    0 hour
+    1 → the 1st day of the month
+    * any month
+    * any day of the week
+ */
 
 function leaveUpdater() {
     cron.schedule("0 0 1 * *", async () => {
